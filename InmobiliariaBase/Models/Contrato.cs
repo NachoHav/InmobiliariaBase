843e1beaf1925e47db28b007c1d61d89857f1822
Nacho <nachohavelka@gmail.com>
2021-04-10 17:03:33 -0300

    Modificacion al inmueble y creacion del modelo y bd de contratos

---
+++ InmobiliariaBase/Models/Contrato.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InmobiliariaBase.Models
+{
+    public class Contrato
+    {
+
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+        public DateTime FechaDesde { get; set; }    
+        public DateTime FechaHasta { get; set; }
+
+        public bool Estado { get; set; }
+
+        [Display(Name = "Inmueble")]
+        public int InmuebleId { get; set; }
+
+
+        [Display(Name = "Inquilino")]
+        public int InquilinoId { get; set; }
+
+        public Inquilino Inquilino { get; set; }
+
+        public Inmueble Inmueble { get; set; }
+    }
+}
